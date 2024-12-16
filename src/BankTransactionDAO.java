0ea587bdf82ecad7621c141bee30fdfaf493e146
Jinwoo Choi <jk62362@mensakorea.org>
2024-12-16 17:48:49 +0900

    feat: 예제 2-9; 정보 응집 예제

---
+++ src/BankTransactionDAO.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+// DAO(data access object): 데이터 접근 객체
+public class BankTransactionDAO {
+
+    public BankTransaction create(final LocalDate date, final double amount, final String description) {
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction read(final long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction update(final long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void delete(final BankTransaction bankTransaction) {
+        throw new UnsupportedOperationException();
+    }
+}
