1d185ee1a49a5cab78cf07f28602dd8f8b87e833
Jinwoo Choi <jk62362@mensakorea.org>
2024-12-18 15:56:29 +0900

    feat: 예제 2-13; 메인 응용프로그램

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
     }
 }
