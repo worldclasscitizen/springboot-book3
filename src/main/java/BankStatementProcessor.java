91ce4b1a172fc8ffe4a239124e70e3ea52889967
teddynu <jk62362@mensakorea.org>
2024-12-19 21:03:50 +0900

    feat: 예제 3-11; BankStatementProcessor 클래스의 핵심 연산

--- src/main/java/BankStatementProcessor.java
+++ src/main/java/BankStatementProcessor.java
@@ -12,32 +12,16 @@ public class BankStatementProcessor {
         this.bankTransactions = bankTransactions;
     }
 
-    public double calculateTotalAmount() {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            total += bankTransaction.getAmount();
+    public double summarizeTransactions(final BankTransactionSummarizer bankTransactionSummarizer) {
+        double result = 0;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
         }
-        return total;
+        return result;
     }
 
     public double calculateTotalInMonth(final Month month) {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDate().getMonth() == month) {
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
-    }
-
-    public double calculateTotalForCategory(final String category) {
-        double total = 0;
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDescription().equals(category)) {
-                total += bankTransaction.getAmount();
-            }
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransaction) -> bankTransaction.getDate().getMonth() == month ? acc + bankTransaction.getAmount() : acc);
     }
 
     public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
@@ -48,5 +32,9 @@ public class BankStatementProcessor {
             }
         }
         return result;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 }
---
+++ src/main/java/BankTransactionSummarizer.java
@@ -0,0 +1,6 @@
+package main.java;
+
+@FunctionalInterface
+public interface BankTransactionSummarizer {
+    double summarize(double accumulator, BankTransaction bankTransaction);
+}
